1862fd25d621453f996ec6f53c952a7737feb11b
Alejandro Espinoza - Oficina <alejandro.eh30@gmail.com>
2025-09-15 20:59:06 -0600

    feat(backend-routing): agregar rutas para registro, reseteo de contraseña y login

--- public/index.php
+++ public/index.php
@@ -2,10 +2,25 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\Logincontroller;
 use MVC\Router;
 $router = new Router();
 
+// Login
+$router->get('/', [Logincontroller::class, 'login']);
+$router->post('/', [Logincontroller::class, 'login']);
 
+//Crear cuenta
+$router->get('/create', [Logincontroller::class, 'create']);
+$router->post('/create', [Logincontroller::class, 'create']);
+
+//Resetear Password
+$router->get('/reset', [Logincontroller::class, 'reset']);
+$router->post('/reset', [Logincontroller::class, 'reset']);
+
+//Mensaje 
+$router->get('/mensaje', [Logincontroller::class, 'mensaje']);
+$router->get('/confirmar', [Logincontroller::class, 'confirmar']);
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
