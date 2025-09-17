55e6d09bfeb8ddc6cdaadea04991242a678fbf68
Alejandro Espinoza - Oficina <alejandro.eh30@gmail.com>
2025-09-17 15:50:06 -0600

    feat(frontend-login): agregar estructura inical vista login

---
+++ views/auth/login.php
@@ -0,0 +1,32 @@
+<div class="contenedor">
+    <h1>UpTask</h1>
+    <p>Crea y administra tus proyectos</p>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Iniciar Sesión</p>
+        <form action="/" method="post" class="formulario">
+            <div class="campo">
+                <label for="email">Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Tu email"
+                    name="email">
+            </div>
+            <div class="campo">
+                <label for="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Tu password"
+                    name="password">
+            </div>
+
+            <input type="submit" class="boton">
+        </form>
+        <div class="acciones">
+            <a href="/create">¿Aún no tienes una cuenta? Crea una</a>
+            <a href="/reset">¿Olvidaste tu password?</a>
+        </div>
+    </div>
+</div>
