8d72edda08c27ba306fe3d60cd6ccfc67f9718fe
Alejandro Espinoza - Oficina <alejandro.eh30@gmail.com>
2025-11-01 13:36:49 -0600

    feat(backend-restablecer-password): agregar método restablecer, mensaje, confirmar

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,16 +1,18 @@
 <?php
 
 namespace Controllers;
+
 use MVC\Router;
 
 
-class Logincontroller {
-    public static function login(Router $router){
-        if($_SERVER['REQUEST_METHOD'] === 'POST'){
-
+class Logincontroller
+{
+    public static function login(Router $router)
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
         // Render a la vista
-        $router->render('auth/login',[
+        $router->render('auth/login', [
             'titulo' => 'Iniciar Sesión'
         ]);
     }
@@ -23,18 +25,33 @@ class Logincontroller {
             'titulo' => 'Crear Cuenta'
         ]);
     }
-    public static function reset(Router $router){
+    public static function reset(Router $router)
+    {
 
-        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
         $router->render('auth/reset', [
             'titulo' => 'Restablecer Password'
         ]);
     }
-    public static function mensaje(){
-        echo "Desde mensaje";
+    public static function restablecer(Router $router)
+    {
+
+        $router->render('auth/restablecer', [
+            'titulo' => 'restablecer password'
+        ]);
     }
-    public static function confirmar(){
-        echo "Desde confirmar";
+    public static function mensaje(Router $router)
+    {
+
+        $router->render('auth/mensaje', [
+            'titulo' => 'Cuenta creada Exitosamente'
+        ]);
     }
-} 
+    public static function confirmar(Router $router)
+    {
+        $router->render('auth/restablecer', [
+            'titulo' => 'Confirma tu cuenta UpTask'
+        ]);
+    }
+}
